ab60f6429c7b96424c1244e3f89345395f65452c
kiku ishimoto <kikuish@gmail.com>
2020-05-05 21:39:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,10 @@
 def reverse_each_word(sentence)
-  arr = sentence.split(',')
-  puts arr
+  arr = sentence.split(" ")
+  return_array = []
+
+  arr.each do |word|
+    return_array << word.reverse
+  end
+
+  return return_array.split(" ")
 end
